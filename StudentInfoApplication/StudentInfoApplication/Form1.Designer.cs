88cd771d2bb6192be8ed0b6b43186eb2db3be7c9
cr4zysayo <jgacis14@gmail.com>
2024-10-24 14:21:45 +0800

    Add files via upload

---
+++ StudentInfoApplication/StudentInfoApplication/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace StudentInfoApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Student_id = new System.Windows.Forms.TextBox();
+            this.Lname = new System.Windows.Forms.TextBox();
+            this.Fname = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.StudentList = new System.Windows.Forms.ListBox();
+            this.Fnamelist = new System.Windows.Forms.ListBox();
+            this.Lnamelist = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Student_id
+            // 
+            this.Student_id.Location = new System.Drawing.Point(12, 47);
+            this.Student_id.Name = "Student_id";
+            this.Student_id.Size = new System.Drawing.Size(170, 20);
+            this.Student_id.TabIndex = 0;
+            this.Student_id.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // Lname
+            // 
+            this.Lname.Location = new System.Drawing.Point(204, 47);
+            this.Lname.Name = "Lname";
+            this.Lname.Size = new System.Drawing.Size(170, 20);
+            this.Lname.TabIndex = 1;
+            this.Lname.TextChanged += new System.EventHandler(this.textBox1_TextChanged_2);
+            // 
+            // Fname
+            // 
+            this.Fname.Location = new System.Drawing.Point(399, 47);
+            this.Fname.Name = "Fname";
+            this.Fname.Size = new System.Drawing.Size(170, 20);
+            this.Fname.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Student ID*";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(200, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Last Name*";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(395, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "First Name*";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(485, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 35);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Submit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // StudentList
+            // 
+            this.StudentList.FormattingEnabled = true;
+            this.StudentList.Location = new System.Drawing.Point(12, 141);
+            this.StudentList.Name = "StudentList";
+            this.StudentList.Size = new System.Drawing.Size(170, 173);
+            this.StudentList.TabIndex = 9;
+            this.StudentList.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Fnamelist
+            // 
+            this.Fnamelist.FormattingEnabled = true;
+            this.Fnamelist.Location = new System.Drawing.Point(204, 141);
+            this.Fnamelist.Name = "Fnamelist";
+            this.Fnamelist.Size = new System.Drawing.Size(170, 173);
+            this.Fnamelist.TabIndex = 10;
+            // 
+            // Lnamelist
+            // 
+            this.Lnamelist.FormattingEnabled = true;
+            this.Lnamelist.Location = new System.Drawing.Point(399, 141);
+            this.Lnamelist.Name = "Lnamelist";
+            this.Lnamelist.Size = new System.Drawing.Size(170, 173);
+            this.Lnamelist.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Student ID List";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(395, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Last Name List";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(200, 118);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "First Name List";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 323);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Lnamelist);
+            this.Controls.Add(this.Fnamelist);
+            this.Controls.Add(this.StudentList);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Fname);
+            this.Controls.Add(this.Lname);
+            this.Controls.Add(this.Student_id);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Student_id;
+        private System.Windows.Forms.TextBox Lname;
+        private System.Windows.Forms.TextBox Fname;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox StudentList;
+        private System.Windows.Forms.ListBox Fnamelist;
+        private System.Windows.Forms.ListBox Lnamelist;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ StudentInfoApplication/StudentInfoApplication/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentInfoApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class StudentInfo
+        {
+            private string studentID;
+            private string lastName;
+            private string firstName;
+
+            public StudentInfo() { }
+
+            public StudentInfo(string id, string lastName, string firstName)
+            {
+                this.studentID = id;
+                this.lastName = lastName;
+                this.firstName = firstName;
+            }
+
+            public string StudentID
+            {
+                get { return studentID; }
+                set { studentID = value; }
+            }
+
+            public string LastName
+            {
+                get { return lastName; }
+                set { lastName = value; }
+            }
+
+            public string FirstName
+            {
+                get { return firstName; }
+                set { firstName = value; }
+            }
+            public StudentInfo(string id, string lastName)
+            {
+                this.studentID = id;
+                this.lastName = lastName;
+                this.firstName = firstName;
+            }
+
+            
+        }
+    private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StudentInfo student = new StudentInfo(
+                Student_id.Text,
+                Lname.Text,
+                Fname.Text
+            );
+
+            StudentList.Items.Add(student.StudentID);
+            Lnamelist.Items.Add(student.LastName);
+            Fnamelist.Items.Add(student.FirstName);
+
+            Student_id.Clear();
+            Lname.Clear();
+            Fname.Clear();
+        }
+    }
+    }
+
+//James Vladimir Z. Gacis
